584360c47ed23b874a3c66c40f810a7b8f02a779
rafael-m1 <rafael.muzzi2@gmail.com>
2025-07-07 16:27:35 -0400

    feat: adiciona metodo excluir

--- excluir-produto.php
+++ excluir-produto.php
@@ -1,7 +1,8 @@
 <?php
-require "src/conexao-bd.php";
+    require "src/conexao-bd.php";
     require "src/Modelo/Produto.php";
     require "src/Repositorio/ProdutoRepositorio.php";
 
-var_dump($_GET);
-echo $_GET["id"];
+    $produtoRepository = new ProdutoRepositorio($pdo);
+    $id = $_GET["id"];
+    $produtoRepository->excluir($id);
